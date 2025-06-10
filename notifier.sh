3d9f3959c018bae9b4003e235ac1a607874edc13
Alexander S. <204044518+alex-dv9@users.noreply.github.com>
2025-06-10 21:58:33 +0300

    Create notifier.sh

---
+++ notifier.sh
@@ -0,0 +1,18 @@
+#!/bin/ash
+
+TOKEN=""
+CHAT_ID=""
+
+PATTERN="authpriv\.info|authpriv\.notice|daemon\.err uhttpd"
+
+send_to_telegram() {
+  MESSAGE=$1
+  URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHATI_ID}&text=$(echo "$MESSAGE" | sed 's/ /%20/g')"
+  wget -qO- "$URL" > /dev/null
+}
+
+while IFS= read -r line: do
+  if echo "$line" | grep -qE "$PATTERN"; then
+    send_to_telegram "$line"
+  fi
+done
