fe3cd010f5997614ae9c154acf6e1509afea4236
Alexander S. <204044518+alex-dv9@users.noreply.github.com>
2025-06-10 21:43:01 +0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,24 +1,34 @@
+import sys
+import re
 import urllib.parse
 import urllib.request
 import json
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("logString")
-args = parser.parse_args()
+pattern = re.compile("authpriv\.info|authpriv\.notice|daemon\.err uhttpd") # it will grep only ssh session start and close, ssh login with publickey and luci successful logins
+def notifier(messageToSend):
+  # enter your bot token and telegram account (or group) id
+  token=""
+  chatid=""
 
-# paste your bot token and telegram user id here 
-token=""
-chatid=""
+  url=f"https://api.telegram.org/bot{token}/sendMessage"
+  message=urllib.parse.urlencode({
+    "chat_id": chatid,
+    "text": messageToSend
+  }).encode("utf-8")
 
-url=f"https://api.telegram.org/bot{token}/sendMessage"
-message= urllib.parse.urlencode({
-  "chat_id": chatid,
-  "text": args.logString
-}).encode("utf-8")
+  request = urllib.request.Request(url, data=message)
 
-request = urllib.request.Request(url, data=message)
+  try:
+    with urllib.request.urlopen(request) as responce:
+      result=responce.read()
+      print(json.loads(result))
+  except Exception as e:
+    print("something went wrong") # this log is useless because you can't really read it. Replace it with subprocess and loger (openwrt) call if you need
 
-with urllib.request.urlopen(request) as responce:
-  result = responce.read()
-  print(json.loads(result))
+def main():
+  for log_line in sys.stdin():
+    if pattern.search(log_line):
+      notifier(messageToSend=log_line)
+  
+if __name__ == "__main__":
+  main()
