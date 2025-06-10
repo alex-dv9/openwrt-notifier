bb1336f7df0417a9712d2adbd81518cb77307cc8
Alexander S. <204044518+alex-dv9@users.noreply.github.com>
2025-06-10 22:29:02 +0300

    Update notifier.sh

--- notifier.sh
+++ notifier.sh
@@ -7,7 +7,7 @@ PATTERN="authpriv\.info|authpriv\.notice|daemon\.err uhttpd"
 
 send_to_telegram() {
   MESSAGE=$1
-  URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=$(echo "$MESSAGE" | sed 's/ /%20/g')"
+  URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=$(echo "$MESSAGE" | sed 's/ /%20/g')" # sed trick can cause some issues but it's working flawlessly in specific case so I don't really care
   wget -qO- "$URL" > /dev/null
 }
 
