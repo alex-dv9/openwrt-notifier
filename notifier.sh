99b82f78ff31edcd3862c2567e7a328ec46b0500
Alexander S. <204044518+alex-dv9@users.noreply.github.com>
2025-06-10 22:15:53 +0300

    Update notifier.sh

--- notifier.sh
+++ notifier.sh
@@ -7,11 +7,11 @@ PATTERN="authpriv\.info|authpriv\.notice|daemon\.err uhttpd"
 
 send_to_telegram() {
   MESSAGE=$1
-  URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHATI_ID}&text=$(echo "$MESSAGE" | sed 's/ /%20/g')"
+  URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=$(echo "$MESSAGE" | sed 's/ /%20/g')"
   wget -qO- "$URL" > /dev/null
 }
 
-while IFS= read -r line: do
+while IFS= read -r line; do
   if echo "$line" | grep -qE "$PATTERN"; then
     send_to_telegram "$line"
   fi
